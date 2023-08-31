e2b3534e92f4384061bca1e5020fd9e4baef2456
Alexander <a.ayugov@zebrains.team>
2023-08-31 18:14:15 +0400

    adding effects for cards and button

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,49 @@ const App = () => {
           <h1 className="title">All Notes</h1>
           <div className="cards">
             <div className="card">
-              <h3 className="card-title">name of note</h3>
-              <button className="card-button">&#8250;</button>
-              <p className="card-text">250</p>
+              <div className="card-bottom">
+                <h3 className="card-title">About my day</h3>
+                <button className="card-button">&#8250;</button>
+              </div>
+              <div className="card-bottom">
+                <p className="card-text">250</p>
+                <p className="card-date">25.08.2023</p>
+              </div>
+
             </div>
             <div className="card">
-              <h3 className="card-title">name of note</h3>
-              <button className="card-button">&#8250;</button>
-              <p className="card-text">250</p>
+              <div className="card-bottom">
+                <h3 className="card-title">About my day</h3>
+                <button className="card-button">&#8250;</button>
+              </div>
+              <div className="card-bottom">
+                <p className="card-text">250</p>
+                <p className="card-date">25.08.2023</p>
+              </div>
+
             </div>
             <div className="card">
-              <h3 className="card-title">name of note</h3>
-              <button className="card-button">&#8250;</button>
-              <p className="card-text">250</p>
+              <div className="card-bottom">
+                <h3 className="card-title">About my day</h3>
+                <button className="card-button">&#8250;</button>
+              </div>
+              <div className="card-bottom">
+                <p className="card-text">250</p>
+                <p className="card-date">25.08.2023</p>
+              </div>
+
             </div>
             <div className="card">
-              <h3 className="card-title">name of note</h3>
-              <button className="card-button">&#8250;</button>
-              <p className="card-text">250</p>
+              <div className="card-bottom">
+                <h3 className="card-title">About my day</h3>
+                <button className="card-button">&#8250;</button>
+              </div>
+              <div className="card-bottom">
+                <p className="card-text">250</p>
+                <p className="card-date">25.08.2023</p>
+              </div>
+
             </div>
-            
           </div>
           <button className="button">+</button>
         </div>
