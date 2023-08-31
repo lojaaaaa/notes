bd490216014525959a236c1369d04c3625dc2393
Alexander <a.ayugov@zebrains.team>
2023-08-31 17:48:53 +0400

    create feature/design branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 
-function App() {
 
+const App = () => {
   return (
-    <>
-      <button>dev branch</button>
-    </>
+    <div className="wrapper">
+      <section className="notes">
+        <div className="notes__container container">
+          <h1 className="title">All Notes</h1>
+          <div className="cards">
+            <div className="card">
+              <h3 className="card-title">name of note</h3>
+              <button className="card-button">&#8250;</button>
+              <p className="card-text">250</p>
+            </div>
+            <div className="card">
+              <h3 className="card-title">name of note</h3>
+              <button className="card-button">&#8250;</button>
+              <p className="card-text">250</p>
+            </div>
+            <div className="card">
+              <h3 className="card-title">name of note</h3>
+              <button className="card-button">&#8250;</button>
+              <p className="card-text">250</p>
+            </div>
+            <div className="card">
+              <h3 className="card-title">name of note</h3>
+              <button className="card-button">&#8250;</button>
+              <p className="card-text">250</p>
+            </div>
+            
+          </div>
+          <button className="button">+</button>
+        </div>
+      </section>
+    </div>
   )
 }
 
