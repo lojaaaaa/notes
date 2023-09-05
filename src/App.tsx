b634c285f8c6539f3806ed93758d84cd3017a691
Alexander <a.ayugov@zebrains.team>
2023-09-05 14:20:27 +0400

    add new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 import Card from "./components/Card/Card"
 import Form from "./components/Form/Form";
 import { ICard } from "./types/types";
---
+++ src/utils/getFromLocalStorage.ts
@@ -0,0 +1,8 @@
+export const getFromLocalStorage = (getItemName: string) =>{
+    const storedNotes = localStorage.getItem(getItemName)
+    if (storedNotes !== null){
+      const parsedNotes = JSON.parse(storedNotes)
+      return parsedNotes
+    }
+    return []
+  }
